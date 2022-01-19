389dc2894875a33418f1cb2d50b6bba71ea98451
hiteshbedre <32206192+hiteshbedre@users.noreply.github.com>
2022-01-19 21:22:07 +0530

    added pii

---
+++ pii.py
@@ -0,0 +1,15 @@
+ ),
+        migrations.RemoveField(
+            model_name='patientregistration',
+            name='aadhar_no',
+        ),
+        migrations.AlterField(
+            model_name='facilityrelatedsummary',
+
+          m_cookieBehavior = CachePolicyCookieBehaviorMapper::GetCachePolicyCookieBehaviorForName(StringUtils::Trim(Aws::Utils::Xml::DecodeEscapedXmlText(cookieBehaviorNode.GetText()).c_str()).c_str());
+      m_cookieBehaviorHasBeenSet = true;
+    }
+    XmlNode cookiesNode = resultNode.FirstChild("Cookies");
+    if(!cookiesNode.IsNull())
+    {
+      m_cookies = cookiesNode;
